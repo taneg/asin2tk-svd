f1907c4505cddb423f56154197b30bf44fbc238f
tanxiangsong <838958665@qq.com>
2020-08-13 09:29:59 +0800

    移除冗余代码

--- src/main/java/com/kalpana/asin2tk/svd/controller/SvdController.java
+++ src/main/java/com/kalpana/asin2tk/svd/controller/SvdController.java
@@ -1,9 +1,7 @@
 package com.kalpana.asin2tk.svd.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
